4cba1bab62ed82f0537d8a2e07e9f830d2f7418f
SiddharthPimprikar <siddharth.pimprikar@apexon.com>
2025-06-04 16:48:02 +0530

    Update wsgi.py
    Changes in wsgi for uppercasing

--- Data_Quality_check/wsgi.py
+++ Data_Quality_check/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'data_quality_check.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', ''Data_Quality_check.settings')
 
 application = get_wsgi_application()
